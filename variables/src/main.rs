5c6eb12f66b046e282fa2fb1dc17251ef0904e9f
memphis242 <abdullah.almosalami.eng@gmail.com>
2023-11-28 11:51:08 -0600

    Chapter 3: Shadowing immutable variables, mimicking mutability, demonstrating how scope and shadowing interact.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;  // Will cause an error since x is immutable by default!
-    println!("The value of x is: {x}");
+    let x = 5;
+    println!("The value of x at the beginning is: {x}");
+    let x = x + 1;  // shadowing x
+    println!("The value of x after the first shadow is: {x}");
+
+    {
+        let x = x * 2;  // shadowing x again
+        println!("The value of x in the inner scope with the second shadow is: {x}");
+    }
+
+    println!("The value of x after the inner scope is: {x}");
 }
